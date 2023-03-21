2fab976eec7367e7249220b09e8bc08aaa42799f
avrians <avriansyahb10@gmail.com>
2023-03-21 11:31:37 +0700

    update add classroom (seed)

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ClassRoom;
 
 class ClassController extends Controller
 {
-    //
+    public function index()
+    {
+        $class = ClassRoom::all();
+        return view('classroom', ['classList'=> $class]);
+    }
 }
