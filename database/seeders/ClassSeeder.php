01aa33695877c65c4cebf097b285e723b1abefc6
avrians <avriansyahb10@gmail.com>
2023-03-23 16:42:09 +0700

    seeder tabel class

--- database/seeders/ClassSeeder.php
+++ database/seeders/ClassSeeder.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\ClassRoom;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Schema;
+
 
 class ClassSeeder extends Seeder
 {
@@ -16,10 +18,25 @@ class ClassSeeder extends Seeder
      */
     public function run()
     {
-        ClassRoom:: insert([
-            'name'=> 'Aiu',
-            'created_at'=> Carbon::now(),
-            'updated_at'=> Carbon::now()
-        ]);
+        Schema::disableForeignKeyConstraints();
+        ClassRoom::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $data = [
+            ['name'=> '1A'],
+            ['name'=> '1B'],
+            ['name'=> '1C'],
+            ['name'=> '1D']
+        ];
+
+        foreach($data as $value) {
+            ClassRoom:: insert([
+                'name'=> $value['name'],
+                'created_at'=> Carbon::now(),
+                'updated_at'=> Carbon::now()
+            ]);
+        }
+
+        
     }
 }
